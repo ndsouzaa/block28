817d9f6548a835e2dac9b214678afc3574bdc711
Neha <nehad2000@gmail.com>
2023-11-09 13:41:49 -0600

    finished workshop

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+// import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css'
+import Blue from './components/Blue';
+import Red from './components/Red';
+import Home from './components/Home';
+
+
+
+function App() {
+
+  return (
+    <>
+      <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">
+      <Link to='/blue'>Blue</Link>
+      <Link to='/red'>Red</Link>
+      <Link to='/home'>Home</Link>
+
+
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/blue' element={<Blue/>}/>
+          <Route path='/red' element={<Red/>}/>
+          <Route path='/Home' element={<Home/>}/>
+
+
+        </Routes>
+        </div>
+    </div>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+function Home(){
+
+  return(
+    <>
+    <div className='home'>
+
+    <h1> HOME</h1>
+    </div>
+    </>
+  )
+}
+
+export default Home;
